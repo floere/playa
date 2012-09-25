4bb1da5653368a3b8935501d9f63bfb4c35c1584
Florian René Hanke <florian.hanke@gmail.com>
2012-09-25 16:35:19 +1000

    + uncruft

--- lib/playa/player.rb
+++ lib/playa/player.rb
@@ -14,7 +14,7 @@ module Playa
     # A mapping of players and their success error codes.
     #
     @@players = {
-      'afplay' => 1, # Yep. It's 1.
+      # 'afplay' => 1, # Yep. It's 1.
       'play'   => 0
     }
     def select_player
@@ -50,11 +50,8 @@ module Playa
         
         file = results.next || return
         loop do
-          silently do
-            options = @@options[self.player]
-            child_pid = spawn self.player, *options, '-v', '0.5', file
-            system "sleep 0.5"
-          end
+          options = @@options[self.player]
+          child_pid = spawn self.player, *options, '-v', '0.5', file
           Process.waitall
           file = results.next unless repeat_one
           break unless file
@@ -83,24 +80,6 @@ module Playa
     def toggle_repeat_one
       Process.kill 'USR2', @current_pid if @current_pid
     end
-    
-    def silently
-        begin
-          orig_stderr = $stderr.clone
-          orig_stdout = $stdout.clone
-          $stderr.reopen File.new('/dev/null', 'w')
-          $stdout.reopen File.new('/dev/null', 'w')
-          retval = yield
-        rescue Exception => e
-          $stdout.reopen orig_stdout
-          $stderr.reopen orig_stderr
-          raise e
-        ensure
-          $stdout.reopen orig_stdout
-          $stderr.reopen orig_stderr
-        end
-        retval
-      end
     
   end
 
