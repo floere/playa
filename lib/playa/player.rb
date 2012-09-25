cb0e251492e25b0fb49a14634dce3f8d10816062
Florian René Hanke <florian.hanke@gmail.com>
2012-09-25 16:27:07 +1000

    + truly player agnostic

--- lib/playa/player.rb
+++ lib/playa/player.rb
@@ -14,8 +14,8 @@ module Playa
     # A mapping of players and their success error codes.
     #
     @@players = {
-      # 'afplay' => 1, # Yep. It's 1.
-      'play' => 0
+      'afplay' => 1, # Yep. It's 1.
+      'play'   => 0
     }
     def select_player
       @player, _ = @@players.find do |(player, success)|
@@ -25,8 +25,12 @@ module Playa
       puts "No suitable player found: tried #{@@players.keys.join(', ')}." unless player
     end
     
-    # Start playing.
+    # Start playing (using player specific options).
     #
+    @@options = {
+      'afplay' => [],
+      'play' => ['-q']
+    }
     def play results
       stop
       
@@ -46,7 +50,11 @@ module Playa
         
         file = results.next || return
         loop do
-          child_pid = spawn self.player, '-v', '0.5', file, '> /dev/null 2>&1'
+          silently do
+            options = @@options[self.player]
+            child_pid = spawn self.player, *options, '-v', '0.5', file
+            system "sleep 0.5"
+          end
           Process.waitall
           file = results.next unless repeat_one
           break unless file
@@ -75,6 +83,24 @@ module Playa
     def toggle_repeat_one
       Process.kill 'USR2', @current_pid if @current_pid
     end
+    
+    def silently
+        begin
+          orig_stderr = $stderr.clone
+          orig_stdout = $stdout.clone
+          $stderr.reopen File.new('/dev/null', 'w')
+          $stdout.reopen File.new('/dev/null', 'w')
+          retval = yield
+        rescue Exception => e
+          $stdout.reopen orig_stdout
+          $stderr.reopen orig_stderr
+          raise e
+        ensure
+          $stdout.reopen orig_stdout
+          $stderr.reopen orig_stderr
+        end
+        retval
+      end
     
   end
 
